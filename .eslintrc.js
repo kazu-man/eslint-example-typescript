e0bd224a805347764e46622f5d6be21210a8f12a
ユーザ <0ht53152387219x@mail.com>
2022-09-19 18:38:36 +0900

    .eslintrc.js 設定修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,22 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
-  overrides: [],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      version: 'latest',
+      version: 'detect',
     },
   },
 };
